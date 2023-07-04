b79baac28eb1b98a3b6a4f993f91e7206d4b5751
Richard Elms <richardelms@gmail.com>
2023-07-04 09:31:33 +0200

    Release v0.1.1

--- Runtime/Scripts/Internal/Version.cs
+++ Runtime/Scripts/Internal/Version.cs
@@ -1,8 +1,8 @@
-﻿namespace BugsnagUnityPerformance
+namespace BugsnagUnityPerformance
 {
     internal static class Version
     {
         //TODO set this using sed or something in the release automation task
-        public const string VersionString = "0.1.0";
+        public const string VersionString = "0.1.1";
     }
 }
